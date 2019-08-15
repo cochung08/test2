9e06c9c58e2804650dbd41766eebf149f8a3dd29
System Administrator <cochung08@gmail.com>
2019-08-16 00:22:42 +0800

    tmp

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cochung08/test2
+
+go 1.12
---
+++ testmod.go
@@ -0,0 +1,5 @@
+package testmod
+main.go main.go import "fmt"
+main.go func SayHello(name string) string {
+   return fmt.Sprintf("Hello, %s", name)
+}
